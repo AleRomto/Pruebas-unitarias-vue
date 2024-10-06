a79e41a757fcc08a5b774a77bd2a95cc6aff88c3
alexi <alexiaromeropinto@gmail.com>
2024-10-05 23:31:56 -0300

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>About Us</h1>
+      <p>Soy un about</p>
+    </div>
+  </template>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Contact Us</h1>
+      <p>soy un Contacto</p>
+    </div>
+  </template>
